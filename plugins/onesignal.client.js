552247e3783b94c8eae684879f88033396f4189f
Lloyd-Aipassa <lloyd.aipassa@gmail.com>
2025-11-21 21:41:32 +0100

    Revert to simple OneSignal config that worked before

--- plugins/onesignal.client.js
+++ plugins/onesignal.client.js
@@ -1,12 +1,5 @@
 export default defineNuxtPlugin(() => {
   if (import.meta.client) {
-    // Register service worker manually first
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/OneSignalSDKWorker.js', { scope: '/' })
-        .then(reg => console.log('OneSignal: Service worker registered manually', reg))
-        .catch(err => console.error('OneSignal: Service worker registration failed', err));
-    }
-
     window.OneSignalDeferred = window.OneSignalDeferred || [];
 
     window.OneSignalDeferred.push(async function(OneSignal) {
@@ -16,9 +9,6 @@ export default defineNuxtPlugin(() => {
       await OneSignal.init({
         appId: appId,
         allowLocalhostAsSecureOrigin: true,
-        serviceWorkerParam: { scope: '/' },
-        serviceWorkerPath: 'OneSignalSDKWorker.js',
-        serviceWorkerUpdaterPath: 'OneSignalSDKUpdaterWorker.js',
         notifyButton: {
           enable: false,
         },
